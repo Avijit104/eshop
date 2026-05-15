09bc991cdbcfb8465ff9183cb339bd50eb3d0095
Avijit Ash <avijitash06@gmail.com>
2026-05-15 20:54:26 +0530

    Revert "added route security in frontend"
    This reverts commit d6f14c4a20643f6a8a9913d4cf5d55573886e8bc.

--- frontend/src/components/Container/SideContainer.jsx
+++ frontend/src/components/Container/SideContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SideContainer({ children }) {
-  return <div className="w-[22%] h-full bg-neutral-800">{children}</div>;
+  return <div className="w-[22%] h-full bg-(--bgSecond) ">{children}</div>;
 }
 
 export default SideContainer;
