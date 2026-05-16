bc199f9bc3f8ac0cb8b3f6fa16259f5087553c15
Avijit Ash <avijitash06@gmail.com>
2026-05-16 23:16:45 +0530

    fixed address validation for business

--- backend/src/validators/seller/business.validator.js
+++ backend/src/validators/seller/business.validator.js
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const businessRegisterValidator = () => {
   return [
@@ -8,7 +9,15 @@ const businessRegisterValidator = () => {
       .withMessage("gst field is required")
       .isLength({ min: 15, max: 15 })
       .withMessage("invalid gst number"),
-    body("address").trim().notEmpty().withMessage("address is required"),
+    body("address")
+      .trim()
+      .notEmpty()
+      .withMessage("address is required")
+      .custom((value) => {
+        if (!mongoose.Types.ObjectId.isValid(value)) {
+          throw new Error("invalid address");
+        }
+      }),
     body("businessName")
       .trim()
       .notEmpty()
